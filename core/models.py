ab27a4db7d8efba340ce4551f2369cfe6b7694cc
Serhiy Skraglenko <skripa88@ukr.net>
2022-05-25 20:52:14 +0300

    Створив класс Visitor

--- core/models.py
+++ core/models.py
@@ -8,7 +8,15 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=128)
     authors = models.ManyToManyField(Author)
+    number = models.IntegerField(default=0)
 
 
 class BookInstance(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+
+class Visitor(models.Model):
+    new_visitor = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book)
+    date = models.DateField()
+
