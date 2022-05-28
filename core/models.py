bc9852d4b396f785238cd4555cf72c7d2c2e04e5
Serhiy Skraglenko <skripa88@ukr.net>
2022-05-28 11:06:55 +0300

    Add fixtures

--- core/models.py
+++ core/models.py
@@ -4,11 +4,17 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=128)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=128)
     authors = models.ManyToManyField(Author)
     number = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.title
 
 
 class BookInstance(models.Model):
