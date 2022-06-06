36cdcae6c5863bca5241814db37b03050bd6b702
Serhiy Skraglenko <skripa88@ukr.net>
2022-06-06 16:25:16 +0300

    Cyange models BookInstance and Visitor

--- core/models.py
+++ core/models.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from unicodedata import name
 from django.db import models
 
 
@@ -13,16 +15,34 @@ class Book(models.Model):
     authors = models.ManyToManyField(Author)
     number = models.IntegerField(default=0)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.title
 
 
 class BookInstance(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    
+    def number_books():
+        instance = int(Book.number - Visitor.number)
+        if instance == 0:
+            return f"All of theese books are in rent!"
+        else:
+            return f"Book rental available" 
 
+    
 
 class Visitor(models.Model):
     name = models.CharField(max_length=200)
+    #order = models.CharField(max_length=200)
+    number = models.ImageField(default=1)
+    #date_added = models.DateTimeField(auto_now_add=True)
+
+    def chek_name():
+        if name == name:
+            pass
+
+    def __str__(self):
+        return self() 
 
 
 class BookRent(models.Model):
@@ -30,3 +50,4 @@ class BookRent(models.Model):
     visitor = models.ForeignKey(Visitor, on_delete=models.CASCADE)
     active = models.BooleanField()
     date = models.DateField()
+
