fb9aebd607dc4e6dcda8021b57ee5eab0c3f64d9
username <luckyhedgehooog@gmail.com>
2022-06-07 17:52:31 +0300

    додала BookForm та def new_book

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Book 
+from .forms import BookForm
 
-# Create your views here.
+def new_book(request):
+    #додати нову книгу
+    if request.method != 'POST':
+        form = BookForm()
+    else:
+        form = BookForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect #куди перенаправляти?:-)
+    
+    context = {'form': form}
+    return render(request, 'core/new_book.html', context)
+
