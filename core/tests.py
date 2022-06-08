61784a4d8268e24b864bd5da2ea5264a6866b753
Bohdan Korniienko <bohdan.korniyenko@flexiana.com>
2022-06-08 17:11:41 +0300

    Refactor number_books + test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,19 @@
+from turtle import title
 from django.test import TestCase
 
-# Create your tests here.
+from core.models import Book, BookInstance, Author
+
+class BookInstanceCase(TestCase):
+    fixtures = ['core/fixtures/library.yaml',]
+
+    def test_number_books(self):
+        """Animals that can speak are correctly identified"""
+
+        books = Book.objects.filter(title__contains='Head first')
+        book = books[0]
+
+        BookInstance.objects.create(book=book)
+        BookInstance.objects.create(book=book)
+    
+        self.assertEqual(book.number_books(), 2)
+        
