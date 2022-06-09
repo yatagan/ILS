9f7d11d1196ace2ef61dee8c359ac739d513c700
username <luckyhedgehooog@gmail.com>
2022-06-09 16:56:23 +0300

    Create main page

--- core/views.py
+++ core/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render,redirect
 from .models import Book 
 from .forms import BookForm
 
+def mylib(request):
+    return render(request, 'core/my_lib.html')
+
 def new_book(request):
     #додати нову книгу
     if request.method != 'POST':
@@ -10,7 +13,7 @@ def new_book(request):
         form = BookForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect #куди перенаправляти?:-)
+            return redirect #куди перенаправляти?
     
     context = {'form': form}
     return render(request, 'core/new_book.html', context)
