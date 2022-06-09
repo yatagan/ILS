9184e7fd9c59583c97b4ef21f8accb52b13ce616
likaantonenko <106235173+likaantonenko@users.noreply.github.com>
2022-06-09 19:56:03 +0300

    Update urls.py

--- ILS/urls.py
+++ ILS/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('ILS.urls')),
+    path('', include('ILS.urls')),
+    path('/warehouse', include('warehouse.urls')),
 ]
