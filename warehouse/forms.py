1953a1d9d08b21a06e974071a425eb3a048bfe84
username <luckyhedgehooog@gmail.com>
2022-06-09 19:52:19 +0300

    Some changes

---
+++ warehouse/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from ..core.models import BookInstance
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = BookInstance
+        fields = ['book', 'ISBN']
--- warehouse/views.py
+++ warehouse/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Book 
+from ..warehouse.forms import BookForm
 
 def index(request):
     #warehouse`s main page`
     return render(request, 'warehouse/index.html')
+
+def new_book(request):
+    #додати нову книгу
+    if request.method != 'POST':
+        form = BookForm()
+    else:
+        form = BookForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('warehouse:index')
+    
+    context = {'form': form}
+    return render(request, 'warehouse/new_book.html', context)
