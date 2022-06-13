a709ad0adf411249022bbdc5537ca0980b861c0a
username <luckyhedgehooog@gmail.com>
2022-06-13 18:12:09 +0300

    add changes

--- warehouse/views.py
+++ warehouse/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render,redirect
-from core.models import BookInstance
 from .forms import BookForm
 
 def index(request):
@@ -8,13 +7,15 @@ def index(request):
 
 def new_book(request):
     #додати нову книгу
+    #books = BookInstance.objects.get(id=book_id)
     if request.method != 'POST':
         form = BookForm()
     else:
         form = BookForm(data=request.POST)
         if form.is_valid():
             form.save()
+            #for book in books:
+                #book = form.save()
             return redirect('warehouse:index')
-    
     context = {'form': form}
     return render(request, 'warehouse/new_book.html', context)
