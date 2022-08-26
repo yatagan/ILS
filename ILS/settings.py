7149896e871fd0ec93734cb9ed0d24a43e7221a8
Bohdan Korniienko <bohdan.korniyenko@flexiana.com>
2022-08-26 15:05:55 +0300

    heroku

--- ILS/settings.py
+++ ILS/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-#STATICFILES_DIRS = [
-#    BASE_DIR / "static",
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 #    '/var/www/static/',
-#]
+]
 
 
 # Default primary key field type
