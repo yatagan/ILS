249c093baae917328c51aaa073441724565a414e
username <luckyhedgehooog@gmail.com>
2022-09-06 19:31:48 +0300

    create search

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,7 @@ app_name = 'core'
 urlpatterns = [
     #Home page
     path('', views.index, name='index'),
+    #сторінка з книгами
+    path('books/', views.books, name='books'),
+    # path('new_order/', views.new_order, name='new_order'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import Book, Post
-from .forms import BookForm
+from .models import Book, BookInstance, Post
+from django.db.models import Q
+
 
 
 def index(request):
@@ -8,4 +9,26 @@ def index(request):
     posts = Post.objects.all()
     return render(request, 'core/index.html', {'posts': posts})
 
-    
+def books(request):
+    search_query = request.GET.get('search', '')
+    if search_query:
+        books = Book.objects.filter(Q(title__icontains=search_query) | Q(authors__name__icontains=search_query))
+    else:
+    #show all books
+        books = Book.objects.order_by('-date_added')
+
+    context = {'books': books}
+    return render(request, 'core/books.html', context)
+
+# def new_order(request):
+#     book = BookInstance.objects.get()
+#     if book:
+#         form = OrderForm(request.POST)
+#         # if form.is_valid():
+#         #     new_order = form.save()
+#         #     new_order.book = book
+#         #     new_order = save()
+        
+  
+#     context = {'order': order, 'form': form}
+#     return render(request, 'core/new_order.html', context)
