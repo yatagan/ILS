b984ae8db7bf7e376c48781a05b0e3fbdf905a59
Bohdan Korniienko <bohdan.korniyenko@flexiana.com>
2022-09-10 10:38:49 +0300

    add rack title

--- warehouse/models.py
+++ warehouse/models.py
@@ -4,6 +4,7 @@ from core.models import BookInstance
 # Create your models here.
 class  Rack(models.Model):
     books = models.ManyToManyField(BookInstance)
+    title = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.number_rack    
