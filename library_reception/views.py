b00680229a724bedb4a51961afdb4c3bcdade3ee
Bohdan Korniienko <bohdan.korniyenko@gmail.com>
2022-10-12 20:15:26 +0300

    Add auth tests (#45)
    * Add auth tests
    
    * Fix tests
    
    * Setting up test action
    
    * Fix ValueError: Missing staticfiles manifest entry for 'core/css/core.css'
    
    * Some fixes
    
    Co-authored-by: Bohdan Korniienko <bohdan.korniyenko@flexiana.com>

--- library_reception/views.py
+++ library_reception/views.py
@@ -4,12 +4,14 @@ from django.shortcuts import render, redirect
 from core.models import BookInstance
 from library_reception.models import BookInstanceRent
 from library_reception.forms import BookInstanceOrderForm, BookInstanceRentForm
+from visitors.models import Librarian
 from .models import BookInstanceOrder
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.http import Http404
+from django.http import HttpResponse, Http404
 
 
+@login_required
 def index(request):
     #library_reception home page
     #context = {'rents': BookInstanceRent.objects.all()}
@@ -28,21 +30,19 @@ def show_order(request):
 
 @login_required
 def book_rent(request):
-    permitions = User.objects.filter(is_staff=1)
-    for legal_user in permitions:
-        if request.user == legal_user:
-            if request.method != 'POST':
-                order_form = BookInstanceRentForm()
-            else:
-                order_form = BookInstanceRentForm(data=request.POST)
-                if order_form.is_valid():
-                    order_form.save()
+    if Librarian.objects.filter(id=request.user.id).exists():
+        if request.method != 'POST':
+            order_form = BookInstanceRentForm()
+        else:
+            order_form = BookInstanceRentForm(data=request.POST)
+            if order_form.is_valid():
+                order_form.save()
                 return redirect('library_reception:index')
-        
-            context = {"order_form":order_form}
-            return render(request, 'library_reception/book_rent.html', context)
+    
+        context = {"order_form":order_form}
+        return render(request, 'library_reception/book_rent.html', context)
     else:
-         raise Http404
+        return HttpResponse('Unauthorized', status=401)
 
 @login_required
 def book_order(request):
