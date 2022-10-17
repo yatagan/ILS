7c097e4d37a4df96d65dc38d6deaf82753c864ce
SerhiySkripa <106245461+SerhiySkripa@users.noreply.github.com>
2022-10-17 09:15:57 +0300

    Continuie compleate book functons (#52)
    * check book function
    
    * add show response order
    
    * add
    
    * add2

--- library_reception/urls.py
+++ library_reception/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('book_rent/', views.book_rent, name='book_rent'),
     path('book_order/', views.book_order, name='book_order'),
-    path('show_order/', views.show_order, name='show_order')
+    path('show_order/', views.show_order, name='show_order'),
+    path('show_response_order/', views.show_response_order, name='show_response_order'),
 ]
