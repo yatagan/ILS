adf0afc2d723a4ce7fa795d6512672a0bc84ad24
SerhiySkripa <106245461+SerhiySkripa@users.noreply.github.com>
2022-11-15 21:48:11 +0200

    Rename models library_reception

--- library_reception/urls.py
+++ library_reception/urls.py
@@ -5,9 +5,9 @@ app_name = 'library_reception'
 urlpatterns = [
     # Library reception home page
     path('', views.index, name='index'),
-    path('book_rent/', views.book_rent, name='book_rent'),
-    path('book_order/', views.book_order, name='book_order'),
-    path('show_order/', views.show_order, name='show_order'),
-    path('rent_book_reserved/', views.rent_book_reserved, name='rent_book_reserved'),
+    path('book_lending/', views.book_lending, name='book_lending'),
+    path('book_reservation/', views.book_reservation, name='book_reservation'),
+    path('show_reservation/', views.show_reservation, name='show_reservation'),
+    path('lending_book_reserved/', views.lending_book_reserved, name='lending_book_reserved'),
     path('check_time_order', views.check_time_order, name='check_time_order'),
 ]
